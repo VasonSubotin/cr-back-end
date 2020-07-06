3a3ec2bf7ff0e4514d9dabfc7bb67d1ee60c348a
mgerbabayan <1babayan>
2020-07-06 04:18:59 -0500

    [ Mger ] prototyping of events generation and sessions

--- client/src/main/java/com/sm/client/services/EventGeneratorService.java
+++ client/src/main/java/com/sm/client/services/EventGeneratorService.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.plugin2.message.Serializer;
 
 import java.io.Serializable;
 import java.util.Date;
