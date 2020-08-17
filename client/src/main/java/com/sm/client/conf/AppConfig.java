94287adf7a687863a066975d4ec30cb05ad9fa2b
mgerbabayan <1babayan>
2020-08-16 23:32:11 -0500

    [ Mger ] Add custom configuration for properties file security

--- client/src/main/java/com/sm/client/conf/AppConfig.java
+++ client/src/main/java/com/sm/client/conf/AppConfig.java
@@ -1,6 +1,11 @@
 package com.sm.client.conf;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.jasypt.iv.RandomIvGenerator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,4 +13,20 @@ import org.springframework.context.annotation.Configuration;
 @EnableEncryptableProperties
 @ComponentScan(basePackages = {"com.sm.client","com.sm.dao"})
 public class AppConfig {
+
+    @Bean(name = "encryptConfig")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(System.getenv("jasypt_encryptor_password"));
+        config.setAlgorithm(System.getenv("jasypt_encryptor_algorithm"));
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setIvGenerator(new RandomIvGenerator());
+       // config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
 }
